502dccdbe60e367318b0d75b9c58cfe7aa311035
cqr <763019675@qq.com>
2017-08-14 14:47:31 +0800

    gradle

---
+++ src/main/java/com/cqr/web/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.cqr.web;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@EnableAutoConfiguration
+public class HelloWorldTest {
+	 @RequestMapping("/hello")
+	    public String index() {
+	        return "Hello World";
+	    }
+}
---
+++ src/test/java/com/cqr/test/ApplicationTests2.java
@@ -0,0 +1,23 @@
+package com.cqr.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cqr.service.BlogProperties;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ApplicationTests2 {
+
+	
+
+	
+
+}
+
